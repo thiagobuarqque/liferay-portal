7e4dc78cd97b933e2c8aa34421d2e28e559e7df0
Thiago Buarque <thiago.buarque@liferay.com>
2026-07-15 21:28:13 -0400

    LPD-98188 Autogenerated (buildREST)

---
+++ modules/apps/portal-language/portal-language-override-rest-impl/src/main/java/com/liferay/portal/language/override/rest/internal/jaxrs/application/PortalLanguageOverrideRESTApplication.java
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.language.override.rest.internal.jaxrs.application;
+
+import jakarta.annotation.Generated;
+
+import jakarta.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Thiago Buarque
+ * @generated
+ */
+@Component(
+	property = {
+		"liferay.jackson=false",
+		"osgi.jaxrs.application.base=/language-override",
+		"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.Vulcan)",
+		"osgi.jaxrs.name=Liferay.Portal.Language.Override.REST"
+	},
+	service = Application.class
+)
+@Generated("")
+public class PortalLanguageOverrideRESTApplication extends Application {
+}
+// LIFERAY-REST-BUILDER-HASH:-880079260
